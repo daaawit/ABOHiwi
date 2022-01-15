8cca147b9979c55faf914017e5f5196c3f6ecced
David Schischke <hi@davidschischke.com>
2022-01-15 11:37:39 +0100

    Simplified function to work with arbitrary list of significance levels

--- R/get_significance_stars.R
+++ R/get_significance_stars.R
@@ -1,22 +1,27 @@
-#' This function takes in a vector of p values
-#' and returns a vector of stars corresponding to
-#' the significance level of the p values
+#' Generate a vector of significance stars for a
+#' set of p values.
+#'
+#' @param p_vals A vector of p values
+#' @param sign_levels A list consisting of the significance levels as names and
+#' significance signs as values. Names must be provided as strings and can have
+#' a leading zero (but don't have to), i.e. can be in form of e.g. ".001" or "0.001".
+#' Significance signs must also be provided as strings.
+#' @return A vector of significance signs corresponding to the p values
 
-get_significance_stars <- function(p_vals){
+get_significance_stars <- function(p_vals, sign_levels = list(".001" = "***", ".01" = "**", ".05" = "*")){
   n <- length(p_vals)
-  
+
+  sign_levels <- sign_levels[sort(names(sign_levels))] # Reordering significance levels in ascending order
+
   stars <- rep("", times = n)
-  
+
   for (i in 1:n){
-    if(p_vals[i] < .001){
-      stars[i] <- "***"
-    } else if (p_vals[i] < .01) {
-      stars[i] <- "**"
-    } else if (p_vals[i] < .05) {
-      stars[i] <- "*"
-    } 
+    for(level in names(sign_levels)){
+      if (p_vals[i] < as.numeric(level)){
+        stars[i] <- sign_levels[[level]]
+        break
+      }
+    }
   }
-  
   return(stars)
 }
-
---
+++ R/palettes.R
@@ -0,0 +1,3 @@
+palettes <- list(
+  colorblind_1 = c("#648fff",  "#ffb000", "#dc267f", "#fe6100", "#785ef0", "#000000", "#FFFFFF", "#999999")
+)
