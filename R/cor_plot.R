c0d7adf220ac1ee6f5c76470640926758bd31561
David Schischke <hi@davidschischke.com>
2022-01-20 10:34:35 +0100

    Added method to excluded nonsignificant correlations from returned table

--- R/cor_plot.R
+++ R/cor_plot.R
@@ -31,27 +31,26 @@
 #' @param sign_levels A list consisting of the significance levels as names and significance signs (e.g. "***") as values. Names must be provided
 #' as strings and can have a leading zero (but don't have to), i.e. can be in form of e.g. ".001" or "0.001". Significance signs must
 #' also be provided as strings. Defaults to .001 = three stars, .01 = two stars, .05 = one star
+#' @param return_significant_only Whether the returned values (table, not the plot) should only contain the correlations that are significant. Defaults 
+#' to FALSE. If set to TRUE, will return any correlation with a p value smaller than the largest supplied significance level. For example, if sign_levels is not modified,
+#' any correlation with p < .1 will be returned. Only works if significance is set to TRUE.
 #' @param CI Whether the CI for each correlation should be reported in the table. Defaults to TRUE.
-#' @param conf_level Confidence level used for the CI. Defaults to 0.95.
+#' @param CI_conf_level Confidence level used for the CI. Defaults to 0.95.
 #' @param plot_CI Whether the CI should be included below the correlation within the plot.
 #' @param colorblind Whether the colors in the plot should be modified to a colorblind palette. Defaults to FALSE. Uses colors from the palette
 #' colorblind_1 in this package if TRUE.
 #' @param title Title that should be added to the plot. If not specified, title will be omitted.
-#' @param label_rotation How many degrees the label on the x axis (corresponds to the values specified as 'y' if y != NULL) should be rotated. 
-#' @param return_plot Whether plot should be returned as an object. If FALSE, plot will be printed
-#' but not saved. If TRUE, function will return a list containing the correlation table and the ggplot data as elements.
-#' 
+#' @param label_rotation How many degrees the label on the x axis (corresponds to the values specified as 'y' if y != NULL) should be rotated.
+#'  
 #' @return Returns the correlation table and plots it as a heatmap. If return_plot is TRUE, also returns the ggplot object as a list element
 #' together with the correlation table. Can be called using print(object$cor_plot)
 #'
 #' @export
 
-
-
 cor_plot <- function(data, x, y = NULL, use = "complete.obs", method = "pearson", digits = 3, digit_size = 4,
-                     significance = T, sign_levels = list(".001" = "***", ".01" = "**", ".05" = "*", ".1" = "."),
-                     CI = T, conf_level = 0.95, plot_CI = T,
-                     colorblind = F, title = NULL, label_rotation = 0, return_plot = F){
+                     significance = T, sign_levels = list(".001" = "***", ".01" = "**", ".05" = "*", ".1" = "."), return_significant_only = F,
+                     CI = T, CI_conf_level = 0.95, plot_CI = T,
+                     colorblind = F, title = NULL, label_rotation = 0){
   
   n <- nrow(data)
   
@@ -75,7 +74,7 @@ cor_plot <- function(data, x, y = NULL, use = "complete.obs", method = "pearson"
 
   if(CI){
     # Relies on psychometric package
-    CIs <- data.frame(t(sapply(cor_plot_data$value, function(x) psychometric::CIr(x,n, level = conf_level))))
+    CIs <- data.frame(t(sapply(cor_plot_data$value, function(x) psychometric::CIr(x,n, level = CI_conf_level))))
     colnames(CIs) <- c("lower", "higher")
 
     cor_plot_data <- cbind(cor_plot_data, CIs)
@@ -114,7 +113,7 @@ cor_plot <- function(data, x, y = NULL, use = "complete.obs", method = "pearson"
   if (significance) labels <- paste(labels, cor_plot_data$sign_level)
   if (plot_CI){
     if(!CI){
-      message("CI is set to false, cannot add it to plot")
+      warning("CI is set to false, cannot add it to plot")
     } else {
       CI_label <- sprintf("\n[%.2f, %.2f]", cor_plot_data$lower, cor_plot_data$higher)
       labels <- paste(labels, CI_label)
@@ -133,6 +132,15 @@ cor_plot <- function(data, x, y = NULL, use = "complete.obs", method = "pearson"
   
   # Rename before exporting
   if(!is.null(y)) colnames(cor_plot_data)[1:3] <- c("IV", "DV", "r") else colnames(cor_plot_data)[1:3] <- c("Var1", "Var2", "r")
-
-  if(return_plot) return(list(cor_data = cor_plot_data, cor_plot = plot)) else return(cor_plot_data)
+  
+  if(return_significant_only){
+    if(!significance){
+      warning("significance is set to FALSE, cannot remove non-significant correlations.")
+    } else {
+      thresh <- as.numeric(max(names(sign_levels)))
+      cor_plot_data <- filter(cor_plot_data, p_val < thresh)
+    }
+  }
+  
+  return(cor_plot_data)
 }
