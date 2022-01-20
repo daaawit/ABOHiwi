a636f9ba8f90efabf1dd75c7340dcae3fe63809c
David Schischke <hi@davidschischke.com>
2022-01-20 10:16:49 +0100

    Updated Distribution plot to show boxplot in same size all the time

--- R/plot_dist.R
+++ R/plot_dist.R
@@ -3,6 +3,7 @@
 #' @import ggplot2
 #' @importFrom jtools theme_apa
 #' @importFrom stats median
+#' @importFrom stats density
 #'
 #' @param var Variable to plot
 #' @param data data.frame in which variable is contained
@@ -16,23 +17,30 @@
 plot_dist <- function(var, data, max_val = NULL, title = "", colorblind = F){
 
   palette <-  if(colorblind) c(palettes$colorblind_1[8], palettes$colorblind_1[4], palettes$colorblind_1[1]) else c("grey", "blue", "red")
-  max_density <- max(as.numeric(prop.table(table(pull(data, var)))))
-
+  if(is.null(max_val)) max_val <- max(pull(data, var))
+  
+  max_density <- max(density(pull(data, var))$y)
+  width <- 0.1 * max_density
+ 
+  
   plot <- ggplot(data, aes(x = pull(data, var = var))) +
     geom_histogram(aes(y=..density..), binwidth = 0.5) +
     geom_density(alpha=.2, fill = palette[1], color = "#878787") +
-
-    geom_boxplot(aes(y = -0.5*max_density), position = "dodge", width = 0.5*max_density) +
+    
+    geom_boxplot(aes(y = 0 - 0.667 * width), position = "dodge", width = width) +
+    
     geom_vline(aes(xintercept = mean(pull(data, var = var), na.rm = T), color = "Mean")) +
     geom_vline(aes(xintercept = median(pull(data, var = var), na.rm = T), color = "Median")) +
 
     scale_color_manual(values = c(palette[2], palette[3])) +
-
+    scale_x_continuous(breaks = seq(1, max_val, by = 1)) + # Step size of 1 on x axis
+  
     xlab("") +
     ylab("") +
     ggtitle(title) +
     theme_apa()
 
-  if(!is.null(max_val)) plot <- plot + xlim(c(0,max_val))
+  
   print(plot)
 }
+
